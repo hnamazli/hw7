b78012575a773e51da82e2c4959e9e6f7a435b60
Hasan <hasan@namazli.com>
2019-10-29 20:02:23 +0400

    fix bug init

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@ const { src, dest, series } = require('gulp');
 
 function html() {
     return src('src/*.html')
-        .pipe(dest('/build'))
+        .pipe(dest('build'))
 }
   
 function css() {
     return src('src/*.css')
-        .pipe(dest('/build'))
+        .pipe(dest('build'))
 }
   
 function js() {
     return src('src/*.js')
-        .pipe(dest('/build'))
+        .pipe(dest('build'))
 }
 
 exports.default = series(html, css, js);
