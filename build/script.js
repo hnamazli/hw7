7fcdf9f38ad6b3f8fe586da2cd633c79fa46b5db
Hasan <hasan@namazli.com>
2019-10-30 18:26:29 +0400

    standart calculate is complete! now working float calculate!

--- build/script.js
+++ build/script.js
@@ -1,12 +1,82 @@
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNum = 0;
+let secondNum = 0;
 let operator = '';
-let flag = false;
+let isSecond = false;
+let isSecOperator = false;
+let input = document.getElementById('screen');
+
+const showValue = value => {
+    input.value = value;
+}
+
+const calculator = () => {
+    let result = 0;
+
+    switch (operator) {
+        case '/':
+            result = secondNum ? firstNum / secondNum : 'Cannot divide by zero';
+            break;
+        case 'x':
+            result = firstNum * secondNum;
+            break;
+        case '+':
+            result = firstNum + secondNum;
+            break;
+        case '-':
+            result = firstNum - secondNum;
+            break;
+        default:
+            result = 0;
+            break;
+    }
+    
+    if (isSecOperator) {    
+        firstNum = result;
+        isSecond = true;
+        return showValue(result);
+    } 
+
+    firstNum = 0;
+    secondNum = 0;
+    operator = '';
+
+    return showValue(result);
+}
+
+const clickOperator = value => {
+    if (!isSecOperator) {
+        operator = value;
+        isSecond = true;
+        isSecOperator = true;
+    } else {
+        calculator();
+        operator = value;
+    }
+}
 
 const allClear = () => {
+    showValue(0);
 
-    firstNumber = 0;
-    secondNumber = 0;
+    firstNum = 0;
+    secondNum = 0;
     operator = '';
-    flag = false;    
+    isSecOperator = false;
+}
+
+const clickKey = value => {
+    if (isSecond) {
+        showValue(0);
+        isSecond = false;
+    }
+
+    let result = input.value + value;
+    let num = parseFloat(result);
+
+    showValue(num);
+
+    if (!operator) {
+        firstNum = num;
+    } else {
+        secondNum = num;
+    }
 }
