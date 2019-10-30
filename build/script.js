94b376cb17b86443f128be68a7e0882a9bfd9e6c
Hasan <hasan@namazli.com>
2019-10-30 16:47:31 +0400

    update js

---
+++ build/script.js
@@ -0,0 +1,12 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let flag = false;
+
+const allClear = () => {
+
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+    flag = false;    
+}
